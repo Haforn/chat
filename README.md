10cb68dcc408e85e061b7eb8ad943d1d9ae9bbe9
Bart Hoekstra <bart-hoekstra@live.nl>
2015-06-09 20:09:37 +0200

    Changed the read me

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # chat
+================
+
+To use the chat add the next lines in the terminal:
+
+================
+
+bundle
+rails g private_pub:install
+rackup private_pub.ru -s thin -E production
---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,16 @@
+class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+
+  def create
+    @message = Message.create!(message_params)
+    PrivatePub.publish_to("/messages/new", message: @message)
+  end
+
+  private
+
+   def message_params
+				params.require(:message).permit(:body)
+   end
+end
